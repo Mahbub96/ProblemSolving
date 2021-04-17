bd78148761cb65e763f54c6ad281a592c9c8d7ea
Mahbub <mahbub@Machine.Limitless>
2021-04-17 17:58:44 +0600

    fixed factorial

--- Others/factorial.cpp
+++ Others/factorial.cpp
@@ -1,36 +1,16 @@
 #include <iostream>
-#include <iomanip>
-#include <limits>
 
 using namespace std;
+int fact(int n){
+	if(n == 1) return 1;
+	return n*fact(n-1);
+}
+
+
 
 int main() {
-    int i = 4;
-    double d = 4.0;
-    string s = "HackerRank ";
-    
-    
-    // Declare second integer, double, and String variables.
-  
-    int newI;
-    double newD;
-    string newS;
-        
-    // Read and save an integer, double, and String to your variables.
-    cin >> newI;
-    cin >> newD;
-    cin.ignore();
-    getline(cin,newS);
-    // Note: If you have trouble reading the entire string, please go back and review the Tutorial closely.
-    
-    // Print the sum of both integer variables on a new line.
-    cout << i+newI << endl;
-    
-    // Print the sum of the double variables on a new line.
-    cout << d+newD << endl;
-    // Concatenate and print the String variables on a new line
-    cout << s+newS << endl;
-    // The 's' variable above should be printed first.
-
+    int n;
+    cin >> n;
+    cout << fact(n) <<endl;
     return 0;
 }
