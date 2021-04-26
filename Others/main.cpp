830edad368d141c40cc4104c77ac76715aa7bbd6
mahbub96 <mahbub@Machine.Limitless>
2021-04-26 12:50:10 +0600

    tracked

--- Others/main.cpp
+++ Others/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdio>
+#include <cstring>
 
 using namespace std;
 
@@ -7,34 +8,31 @@ using namespace std;
 int notaCounter[7];
 int modeaCounter[7];
 
-void calculation(double &n,double *arr,int *counter){
+void calculation(double &n,double* arr,int* counter){
     
     for (int i = 0; i < 6; i++)
-        while(n >= arr[i]){
-            counter[i] += 1;
+        while(n >= arr[i]) { 
+            counter[i] += 1; 
             n -= arr[i];
         }
         
-    
 }
 
-void print(int count[],double arr[]){
+void print(int count[],char* str,double arr[]){
     
-    for (int i = 0; i < 6; i++)
-    {
-        printf("%d nota(s) de R$ %.2lf\n",count[i],arr[i]);
-    }
+    for (int i = 0; i < 6; i++) printf("%d %s %.2lf\n",count[i],str,arr[i]);
+    
     cout << endl;
 }
 
 int main(){
+
     double Notas[] = {100,50,20,10,5,2};
     double Modeas[] = {1,0.5,0.25,0.10,0.05,0.01};
 
     double n;
     
     cin >> n;
-    cout << n;
 
    
     calculation(n,Notas,notaCounter);
@@ -42,9 +40,15 @@ int main(){
     calculation(n,Modeas,modeaCounter);
     
 
-    print(notaCounter,Notas);
+    /*Just Printing sector here*/
+    char str[20];
+    cout << "NOTAS\n";
+    strcpy(str,"nota(s) de R$");
+    print(notaCounter,str,Notas);
+
     cout << "MOEDAS\n";
-    print(modeaCounter,Modeas);
+    strcpy(str,"Moedas(s) de R$");
+    print(modeaCounter,str,Modeas);
 
 
 
---
+++ Others/test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    double n;
+    cin >> n;
+    cout << n;
+    if(n == 12.04) cout << "yes";
+    return 0;
+}
