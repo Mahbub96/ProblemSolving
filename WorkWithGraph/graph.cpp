e221bebf244a912589e0d96e527042bb8cf41b20
mahbub96 <mahbub.alam.sobuz@gmail.com>
2021-08-27 17:07:55 +0600

    generating idea for graph

--- WorkWithGraph/graph.cpp
+++ WorkWithGraph/graph.cpp
@@ -1,19 +1,7 @@
-#include <iostream>
-
-using namespace std;
-
-class Graph
-{
-    int value;
-};
-
-int main()
-{
-<<<<<<< HEAD
-    cout << "this file is under construction" << endl;
-=======
-    int x;
-
->>>>>>> dev
-    return 0;
-}
+/******* 
+ * No Idea
+ * Anti Brain 
+ * Idot 
+ * trying to generate an idea 
+ * pray for me 
+ *******/
