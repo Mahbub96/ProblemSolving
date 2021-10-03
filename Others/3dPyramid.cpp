54249833217472c74f51d892f6a4c1b63c696fce
Machine <mahbub@Mahbub.mahbub>
2021-10-04 02:05:48 +0600

    added class and objects.cpp

--- Others/3dPyramid.cpp
+++ Others/3dPyramid.cpp
@@ -1,13 +1,20 @@
-#include <stdio.h>
-void loader()
+#include <iostream>
+using namespace std;
+// void loader()
+// {
+//     int i;
+//     system("clear");
+//     printf("please wait While Reading Data....\n\n\t");
+//     for (i = 1; i <= 80; i++)
+//     {
+//         Sleep(100);
+//         printf("%c", 219);
+//     }
+// }
+int main()
 {
-    int i;
-    system("clear");
-    printf("please wait While Reading Data....\n\n\t");
-    for(i=1;i<80; i++)
+    for (size_t i = 0; i < 10000000; i++)
     {
-        Sleep(100);
-        printf("%c",219);
-
+        cout << i << endl;
     }
 }
