0d4bf29a36a4a12c3cddb3bece6402841b16c1e2
Machine <mahbub@Mahbub.mahbub>
2021-10-04 15:31:25 +0600

    added Box it

---
+++ Others/Box_it.cpp
@@ -0,0 +1,117 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//Implement the class Box
+//l,b,h are integers representing the dimensions of the box
+class Box
+{
+    int length, bredth, height;
+
+public:
+    // The class should have the following functions :
+
+    // Constructors:
+    // Box();
+    Box()
+    {
+        height = 0;
+        length = 0;
+        bredth = 0;
+    }
+    // Box(int,int,int);
+    Box(int l, int b, int h)
+    {
+        this->length = l;
+        this->bredth = b;
+        this->height = h;
+    }
+    // Box(Box);
+    Box(Box &b)
+    {
+        this->length = b.length;
+        this->bredth = b.bredth;
+        this->height = b.height;
+    }
+    // int getLength(); // Return box's length
+    int getLength() { return length; }
+    // int getBreadth (); // Return box's breadth
+    int getBreadth() { return bredth; }
+    // int getHeight ();  //Return box's height
+    int getHeight() { return height; }
+    // long long CalculateVolume(); // Return the volume of the box
+    long long CalculateVolume() { return height * bredth * length; }
+
+    //Overload operator < as specified
+    //bool operator<(Box& b)
+    bool operator<(Box &b)
+    {
+        if (this->length < b.length)
+            return true;
+        else if (this->bredth < b.bredth && this->length == b.length)
+            return true;
+        else if (this->height < b.height && this->bredth == b.bredth && this->length == b.length)
+            return true;
+        else
+            return false;
+    }
+};
+
+//Overload operator << as specified
+ostream &operator<<(ostream &out, Box &B)
+{
+    // out << B.getHeight
+    out << B.getLength() << " " << B.getBreadth() << " " << B.getHeight() << endl;
+    return out;
+}
+void check2()
+{
+    int n;
+    cin >> n;
+    Box temp;
+    for (int i = 0; i < n; i++)
+    {
+        int type;
+        cin >> type;
+        if (type == 1)
+        {
+            cout << temp << endl;
+        }
+        if (type == 2)
+        {
+            int l, b, h;
+            cin >> l >> b >> h;
+            Box NewBox(l, b, h);
+            temp = NewBox;
+            cout << temp << endl;
+        }
+        if (type == 3)
+        {
+            int l, b, h;
+            cin >> l >> b >> h;
+            Box NewBox(l, b, h);
+            if (NewBox < temp)
+            {
+                cout << "Lesser\n";
+            }
+            else
+            {
+                cout << "Greater\n";
+            }
+        }
+        if (type == 4)
+        {
+            cout << temp.CalculateVolume() << endl;
+        }
+        if (type == 5)
+        {
+            Box NewBox(temp);
+            cout << NewBox << endl;
+        }
+    }
+}
+
+int main()
+{
+    check2();
+}
--- Others/test.cpp
+++ Others/test.cpp
@@ -1,66 +1,27 @@
 #include <iostream>
-#include <vector>
-#include <queue>
-
 using namespace std;
 
-void BFS(vector<vector<int>> &v)
+class Complex
 {
+    int a, b;
 
-    int startingNode;
-    cout << "Enter Starting Node for start BFS : ";
-    cin >> startingNode;
-
-    queue<int> q, output;
-    int currentNodeSize = 0;
-    q.push(startingNode);
-
-    while (q.size())
+public:
+    Complex(int m, int d)
     {
-        int currentNode = q.front();
-
-        int parents = v[currentNode].size();
-
-        for (size_t i = 0; i < v[currentNode].size(); i++)
-        {
-            q.push(v[currentNode][i]);
-            output.push(v[currentNode][i]);
-        }
-
-        cout << currentNode << " ";
-        q.pop();
+        a = m;
+        b = d;
     }
-}
+    friend ostream &operator<<(ostream &os, const Complex &complex);
+};
 
-void input(vector<vector<int>> &v)
+ostream &operator<<(ostream &os, const Complex &complex)
 {
-
-    for (size_t i = 0; i < v.size(); i++)
-    {
-        int child;
-        cout << "\nHow many child for node" << i << " : ";
-        cin >> child;
-        for (size_t j = 0; j < child; j++)
-        {
-            cout << "Enter " << j << "no child for node " << i << " : ";
-            int temp;
-            cin >> temp;
-            v[i].push_back(temp);
-        }
-    }
+    os << complex.a << '+' << complex.b << 'i';
+    return os;
 }
 
 int main()
 {
-    int totalNode;
-    cout << "Enter total Node : ";
-    cin >> totalNode;
-
-    /* input sector */
-    vector<vector<int>> v(totalNode);
-    input(v);
-
-    BFS(v);
-
-    return 0;
+    Complex complex(5, 6);
+    cout << complex;
 }
