d5b7a7c98b48c5184a9ef0f671246cdb0893609d
Machine <mahbub.alam.sobuz@gmail.com>
2021-10-15 19:46:47 +0600

    solved counting valleys

--- Others/test.cpp
+++ Others/test.cpp
@@ -1,54 +1,11 @@
-#include <iostream>
 
-using namespace std;
+#include <stdio.h>
 
-int calculate(int i, int n, int a, int b)
-{
-
-    // check equality
-    int temp = a;
-    if (a > b)
-    {
-        a = b;
-        b = temp;
-    }
-
-    int finalValue = 0;
-    // add A
-    while (n - i - 1)
-    {
-        finalValue += a;
-        n--;
-    }
-
-    // Add B
-    while (i)
-    {
-        finalValue += b;
-        i--;
-    }
-    return finalValue;
-}
-
-int main()
-{
-    int testCase = 1;
-    cin >> testCase;
-    for (size_t x = 0; x < testCase; x++)
-    {
-        int a = 10, b = 100, n = 4;
-        cin >> n >> a >> b;
-
-        for (size_t i = 0; i < n; i++)
-        {
-
-            cout << calculate(i, n, a, b);
-            if (i < n)
-                cout << " ";
-            if (a == b)
-                break;
-        }
-        cout << endl;
-    }
+int main() {
+    // Write C code here
+    if(fork() && fork())
+        fork();
+    printf("Hello ");
+    
     return 0;
 }
---
+++ python/playfare_cipher.py
@@ -0,0 +1,11 @@
+from collections import OrderedDict
+
+
+def playFare(key, plainText):
+    # find duplicate character
+    key = list(OrderedDict.fromkeys(key))
+
+    # make a table
+
+
+playFare("apple", 12323)
