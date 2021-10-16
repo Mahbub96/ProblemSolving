8c69c6b6031aaf7d5c6e3aac56274cb39a579ca6
Machine <mahbub.alam.sobuz@gmail.com>
2021-10-16 11:17:00 +0600

    solve remove duplicates from playfare cipher

--- Others/playfare_cipher.cpp
+++ Others/playfare_cipher.cpp
@@ -1,13 +1,53 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
+
 using namespace std;
+
+// For print table 
+void print(vector<vector<char>> v)
+{
+    cout << "\n\n Tables : " << endl;
+    for (size_t i = 0; i < v.size(); i++)
+    {
+        for (size_t j = 0; j < v[i].size(); j++)
+        {
+            cout << v[i][j] << ' ';
+        }
+        cout << endl;
+    }
+}
+
+string removeDuplicates(string s)
+{
+    int len = s.length();
+    string final;
+    final.push_back(s.at(0));
+
+    for (size_t i = 1; i < len; i++)
+    {
+        bool found = false; 
+        char ch = s[i];
+        for (size_t j = 0; j < final.length(); j++)
+        {
+            if (ch == final[j])
+            {
+                found = true;
+                break;
+            }
+        }
+        if (!found)
+            final.push_back(ch);
+    }
+
+    return final;
+}
 
 void tables(vector<vector<char>> &v, string key)
 {
     char ch = 'A';
     int c = 0;
     int C = 0;
+    /* added special characters and numbers */
     char sampleTable[] = {50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 61, 63, 64, 91, 93, 123, 125, 33, 34, 35, 36, 37, 38, 40, 41, 44, 46};
 
     for (size_t i = 0; i < 9; i++)
@@ -18,6 +58,7 @@ void tables(vector<vector<char>> &v, string key)
         {
             for (size_t j = 0; j < 9; j++)
             {
+                // when all capital laters has been pushed
                 if (c == 26)
                 {
                     v[i].push_back('a');
@@ -49,6 +90,8 @@ void tables(vector<vector<char>> &v, string key)
                 }
             }
         }
+
+	// push all special characters and numbers 
         else
         {
             for (size_t j = 0; j < 9; j++)
@@ -56,37 +99,23 @@ void tables(vector<vector<char>> &v, string key)
                 v[i].push_back(sampleTable[C++]);
             }
         }
-
-        cout << endl;
     }
 }
 
 string playFare(string plainText, string key)
 {
     // Remove Duplicate
-    sort(key.begin(), key.end());
-    key.erase(unique(key.begin(), key.end()), key.end());
+    key = removeDuplicates(key);
 
     // Generate Table
     vector<vector<char>> table(9);
     tables(table, key);
-
-    cout << "\n\n Outputs Are : " << endl;
-    for (size_t i = 0; i < table.size(); i++)
-    {
-        for (size_t j = 0; j < table[i].size(); j++)
-        {
-            cout << table[i][j] << ' ';
-        }
-        cout << endl;
-    }
-
-    cout << key;
+    print(table);
     return " ";
 }
 
 int main()
 {
-    playFare("str", "mahbub");
+    cout << playFare("mahbub", "aabccb") << endl;
     return 0;
 }
