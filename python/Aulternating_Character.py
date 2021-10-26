6f8dd7427132c2ca15f1d89fed65666ae7640808
Machine <mahbub.alam.sobuz@gmail.com>
2021-10-26 23:33:47 +0600

    solved Aulternating charrecter form hackerrank using python also

---
+++ python/Aulternating_Character.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'alternatingCharacters' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
+
+def alternatingCharacters(s):
+    # Write your code here
+    l = len(s)
+    rem = 0
+    for i in range(l-1) :
+        if(s[i] == s[i+1] : rem+=1
+        
+    return rem
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = alternatingCharacters(s)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
