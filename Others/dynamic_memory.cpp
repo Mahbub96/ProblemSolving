d3c66f501cf84e101082688318b73ab87d3f629b
Machine <mahbub.alam.sobuz@gmail.com>
2021-10-28 18:33:06 +0600

    solved dynamic array using c

---
+++ Others/dynamic_memory.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <malloc.h>
+
+void main()
+{
+    int n, q, **arr;
+    scanf("%d %d", &n, &q);
+
+    arr = (int **)malloc(n * sizeof(int *)); //allocated first index size
+
+    for (int i = 0; i < n; i++)
+    {
+        int s;
+        scanf("%d", &s);
+        arr[i] = (int *)malloc(s * sizeof(int *));
+        for (int j = 0; j < s; j++)
+        {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+
+    for (int i = 0; i < q; i++)
+    {
+        int x, y;
+        scanf("%d %d", &x, &y);
+        printf("%d\n", arr[x][y]);
+    }
+}
