6234a1f08dbd3514b5cc8d6f991cbd64b7f8ffbf
Mahbub96 <mahbub.alam.sobuz@gmail.com>
2021-12-07 01:21:10 +0600

    Fully incomplete SJF os Assignment

--- test/test.cpp
+++ test/test.cpp
@@ -1,19 +1,10 @@
 #include <iostream> 
 using namespace std; 
-void test(){ 
-	int sum();
-}
-test::int sum(){
-	return y;
-}
 
 int main()
 {
     int x;
     int y = 10;
     // Write C code here
-    
-    cout << sum() << endl;
-
     return 0;
 }
