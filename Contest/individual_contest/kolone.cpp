33423b3d59619740e5cf49750c39e5e899787f25
mahbub96 <mahbub.alam.sobuz@gmail.com>
2023-03-03 04:54:28 +0600

    solved kolony but it might be some bugs

--- Contest/individual_contest/kolone.cpp
+++ Contest/individual_contest/kolone.cpp
@@ -1,35 +1,65 @@
-#include <algorithm>
 #include <iostream>
 
 using namespace std;
 
 int main() {
 
-  int x, y;
+  int aLen, bLen, t;
   string a, b, s;
-  int t;
 
-  cin >> x >> y >> a >> b >> t;
+  cin >> aLen >> bLen;
+  cin.ignore();
+  cin >> a >> b;
+  cin.ignore();
+  cin >> t;
 
-  sort(a.rbegin(), a.rend());
-
-  s = a + b;
-  int aLen = a.length();
-  int bLen = b.length();
-
-  int diff = abs(aLen - bLen);
-
-  int l = t > aLen + bLen ? aLen + bLen - 1 : t;
-
-  int fp = aLen - 1;
-  for (int i = 0; i < l; i++) {
-
-    for (int j = 0, k = 0; j <= i; j++, k += 2) {
-      swap(s[fp + k], s[fp + 1 + k]);
-    }
-    --fp;
+  for (int i = aLen - 1; i > -1; i--) {
+    s.push_back(a[i]);
   }
-  cout << s;
+  s += b;
+  int l = aLen + bLen;
 
+  char res[l] = {};
+  if (t == 0) {
+    cout << s;
+    return 0;
+  }
+  // for right line
+  for (int i = 0; i < bLen; i++) {
+    int ind = (aLen + i) - t + i;
+
+    if (ind < 0)
+      ind = 0;
+    if (ind >= l)
+      ind = l - 1;
+
+    // find empty cell
+    for (int j = ind; j < l; j++) {
+      if (res[j] == '\0') {
+        res[j] = s[aLen + i];
+        break;
+      }
+    }
+  }
+
+  // for left line
+  for (int i = 0; i < aLen; i++) {
+
+    int ind = (aLen - i - 1) + t - i;
+    ind = ind > (l - 1) ? l - 1 : ind;
+    ind = ind < 0 ? 0 : ind;
+
+    // find empty cell
+    for (int j = ind; j > -1; j--) {
+      if (res[j] == '\0') {
+        res[j] = s[aLen - i - 1];
+        break;
+      }
+    }
+  }
+
+  for (int i = 0; i < l; i++) {
+    cout << res[i];
+  }
   return 0;
 }
