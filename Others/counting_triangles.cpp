247d22e2265f462af83da0122305d083f749a779
mahbub96 <mahbub.alam.sobuz@gmail.com>
2023-05-20 02:56:05 +0600

    not matching output

--- Others/counting_triangles.cpp
+++ Others/counting_triangles.cpp
@@ -3,94 +3,121 @@
 
 using namespace std;
 
-class Bounds {
+class Bounds
+{
 public:
   int ub;
   int lb;
 };
 
-Bounds find_bounds(int *arr, int s, int e, int &a, int &b, int &l) {
+Bounds find_bounds(int *arr, int s, int e, int &a, int &b, int &l)
+{
 
   Bounds bound;
 
   int lowb = abs(arr[a] - arr[b]) + 1;
   int upb = (arr[a] + arr[b]) - 1;
 
-  if (lowb > b) {
+  cout << "lb:" << lowb << ",ub:" << upb << endl;
+
+  if (lowb > b)
+  {
     bound.lb = b + 1;
   }
 
-  if (upb > b) {
+  if (upb > b)
+  {
 
-    while (s <= e) {
+    while (s <= e)
+    {
 
       int mid = (s + e) / 2;
 
-      cout << arr[mid] << endl;
-      if (arr[mid] == upb) {
+      cout << "s:" << s << ",e:" << e << ",upb:" << upb << endl;
+      cout << "ind : " << mid << "," << arr[mid] << endl;
+      if (arr[mid] == upb)
+      {
 
         bound.ub = mid;
         break;
       }
 
-      if (arr[mid] < upb) {
-        s = mid + 1;
-        if (s == e) {
+      if (arr[mid] < upb)
+      {
+        if (s == e)
+        {
           bound.ub = mid + 1;
+          cout << "last step first" << endl;
           break;
         }
+        s = mid + 1;
       }
 
-      if (arr[mid] > upb) {
-        e = mid - 1;
+      if (arr[mid] > upb)
+      {
 
-        if (s == e) { // in case of last step
-
+        if (s == e)
+        { // in case of last step
+          cout << "last step" << endl;
           bound.ub = mid - 1;
           break;
         }
+        e = mid;
       }
     }
 
-    if (upb < e) {
+    if (upb < e)
+    {
     }
   }
   return bound;
 }
-int main() {
+int main()
+{
   /*
-        int t;
-  cin >> t;
+    int t;
+    cin >> t;
 
-  for (int x = 0; x < t; x++) {
-    int n;
-    int times = 0;
-    cin >> n;
-    int arr[n];
+    for (int x = 0; x < t; x++)
+    {
+      int n;
+      int times = 0;
+      cin >> n;
+      int arr[n];
 
-    for (int i = 0; i < n; i++) {
-     cin >> arr[i];
-    }
-
-    sort(arr, arr + n);
-
-    for (int i = 0; i < n - 2; i++) {
-      int a = arr[i];
-      for (int j = i + 1; j < n - 1; j++) {
-        int b = arr[j];
-
-        Bounds bs = find_bounds(arr, i, n - 1, a, b, n);
+      for (int i = 0; i < n; i++)
+      {
+        cin >> arr[i];
       }
+
+      sort(arr, arr + n);
+
+      for (int i = 0; i < n - 2; i++)
+      {
+        int a = arr[i];
+        for (int j = i + 1; j < n - 1; j++)
+        {
+          int b = arr[j];
+
+          Bounds bs = find_bounds(arr, j + 1, n, a, b, n);
+          int temp = bs.ub - bs.lb;
+          cout << bs.lb << " , " << bs.ub << endl;
+          cout << "temp : " << temp << endl;
+          times = temp > 0 ? temp : 0;
+        }
+      }
+      cout << "Case " << x + 1 << ": " << times << endl;
     }
-    cout << "Case " << x + 1 << ": " << times << endl;
-  }
   */
-  int arr[] = {2, 3, 5, 6, 7, 8, 9};
+
+  int arr[] = {1, 2, 3, 4, 5, 6};
   int a = 0;
   int b = 1;
   int l = 6;
 
   Bounds bs = find_bounds(arr, b + 1, l, a, b, l);
+  cout << (bs.ub - bs.lb) + 1 << endl;
   cout << bs.lb << " , " << bs.ub << endl;
+
   return 0;
 }
